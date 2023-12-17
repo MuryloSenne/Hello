488d1e68fd4f5eda19735adaa2017fc0c088d992
MuryloSenne <143774081+MuryloSenne@users.noreply.github.com>
2023-12-17 05:01:38 -0300

    import reflect

--- hello.go
+++ hello.go
@@ -1,12 +1,16 @@
 package main
 
 import "fmt"
+import "reflect" 
 
 func main(){
     var nome string = "Douglas"
     var idade int = 32
     var versao float32 = 1.1
 
-    fmt.Prinln("Olá, sr.", nome, "sua idade é", idade)
-    fmt.Prinln("Este programa está na versao", versao)
+    fmt.Println("Olá, sr.", nome, "sua idade é", idade)
+    fmt.Println("Este programa está na versao", versao)
+
+    fmt.Println("O tipo da variavel nome é", reflect.Typeof(nome))
+
 }
