c30dff9ca884dda71eacf3e31c2b27e0ead4cad1
MuryloSenne <143774081+MuryloSenne@users.noreply.github.com>
2023-12-17 04:50:18 -0300

    Create hello.go
    Começando em go

---
+++ hello.go
@@ -0,0 +1,12 @@
+package mani
+
+import "fmt"
+
+func main(){
+    var nome string = "Douglas"
+    var idade int = 32
+    var versao float32 = 1.1
+
+    fmt.Prinln("Olá, sr.", nome, "sua idade é", idade)
+    fmt.Prinln("Este programa está na versao", versao)
+}
