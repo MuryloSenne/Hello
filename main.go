8d65321a733e41d6d093f3850a87002dcaee869a
MuryloSenne <143774081+MuryloSenne@users.noreply.github.com>
2023-12-18 04:09:17 -0300

    for / http-net

--- main.go
+++ main.go
@@ -2,32 +2,36 @@ package main
 
 import "fmt"
 import "os"
-
+import "net/http"
 
 func main(){
-
     exibiIntroducao()
-    exibeMenu()
-    comando := leComando()
 
-    switch comando{
-    case 1:
-    fmt.Println("Monitorando...")
-    case 2:
-    fmt.Println("Exibindo logs...") 
-    case 0:
-    fmt.Println("Saindo do programa") 
-    os.Exit(0) 
-    default: 
-    fmt.Println("Não conheço este comando") 
-    os.Exit(-1)
+    for{
+        exibeMenu()
+        comando := leComando()
+    
+        switch comando{
+        case 1:
+            iniciarMonitoramento()
+        case 2:
+            fmt.Println("Exibindo logs...") 
+        case 0:
+            fmt.Println("Saindo do programa") 
+            os.Exit(0) 
+        default: 
+            fmt.Println("Não conheço este comando") 
+            os.Exit(-1)
+        }
     }
+
+   
 }
 
 
 func exibiIntroducao(){
     nome := "Senne"
-    versao := 1.1
+    versao := 1.2
     fmt.Println("Olá, sr.", nome)
     fmt.Println("Este programa está na versao", versao)
 }
@@ -44,4 +48,18 @@ func leComando() int {
     fmt.Println("O valor do comando é", comandoLido)
 
     return comandoLido
+}
+
+func iniciarMonitoramento(){
+    fmt.Println("Monitorando...")
+    site := "https://www.alura.com.br/"
+    resp, _ := http.Get(site)
+    //fmt.Println(resp)
+
+    if resp.StatusCode == 200{
+        fmt.Println("Site", site, "foi carregado com sucesso!")
+    }else{
+        fmt.Println("Site", site, "Está com problema, Status code:", resp.StatusCode)
+    }
+
 }
